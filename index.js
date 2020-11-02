0113fc6d0b2ebd4b4f7ae84ff793025ec04aadac
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-02 11:49:50 -0800

    set up index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const session = require("express-session")
+const KnexSessionStore = require("connect-session-knex")(session)
+const db = require("./database/config")
+const usersRouter = require("./users/users-router")
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session({
+	resave: false, // avoid creating sessions that have not changed
+	saveUninitialized: false, // BDPR laws against setting cookies automatically
+	secret: "keep it secret, keep it safe", // use to cryptographyically sign the cookie
+	
+	// store the session data in the database rather than in memory
+	store: new KnexSessionStore({
+		knex: db, // configured instance of knex
+		createtable: true, // if the table does not exist, it will create it automatically
+	}),
+}))
+
+server.use(usersRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
